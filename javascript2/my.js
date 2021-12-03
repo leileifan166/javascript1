e03c5f79b35bb1ef5268f14795a020aefea9995a
範雷雷 <95039519+leileifan166@users.noreply.github.com>
2021-12-03 21:15:47 +0900

    initial
    がんばりましょう

---
+++ javascript2/my.js
@@ -0,0 +1,62 @@
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(2 * 2);
+console.log(4 / 2);
+console.log(5 % 2);//あまりは１です。
+console.log(0.1 + 0.2);
+console.log(0.7 * 0.2);
+var num = 0.1 + 0.2;
+console.log(num == 0.3);//プログラミングの世界が面白いですね
+/* var age = 1;
+num = num + 1;
+console.log(num); */
+
+var age = 10;
+++age;// age = age + 1;
+
+var p = 10;
+console.log(++p + 10);
+
+var ito = 20;
+ito = ito - 1;//19
+console.log(ito); 
+var tomo = 20;
+console.log(tomo-- - 1);
+
+
+var hiro = 20;
+console.log(--hiro - 1);
+
+console.log(3 > 5 && 3 > 2);//false
+console.log(3 > 1 && 3 > 2);//true
+console.log(3 > 5 || 3 > 2);//true
+console.log(!true);//false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
